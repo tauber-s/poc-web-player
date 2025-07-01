21129ba3851598e5cbdb017f4eb5fbf5e006531a
Sandy <tauber.sandy@gmail.com>
2025-07-01 09:40:39 -0300

    setup spring boot

--- src/main/java/com/poc/webplayer/WebPlayerApplication.java
+++ src/main/java/com/poc/webplayer/WebPlayerApplication.java
@@ -3,19 +3,18 @@ package com.poc.webplayer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class WebPlayerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebPlayerApplication.class, args);
 	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@GetMapping("/")
+	public String home() {
+		return "hello world";
 	}
 
 }
